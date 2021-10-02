d5c0ddb2a28fb479fc833552254ccc6d0ea14008
DELL-13\heeji <heejin.jeong@gmx.de>
2021-10-03 03:43:49 +0900

    .gitignore와 pip_install 추가

---
+++ pip_install.bat
@@ -0,0 +1,10 @@
+pip install pyttsx3
+pip install pywin32
+pip install pyupbit
+pip install PyQt5
+pip install psutil
+pip install telegram
+pip install python-telegram-bot
+pip install pandas
+
+echo "please update open api/project path in utility/setting.py"
