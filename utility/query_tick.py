6fb40c38af5807ac05713c2c3a610d5bcba97b71
yuseonho <youseonho@naver.com>
2021-10-02 18:18:26 +0900

    틱데이터 저장용 쿼리 프로세스 추가

---
+++ utility/query_tick.py
@@ -0,0 +1,42 @@
+import sqlite3
+from utility.setting import ui_num, DB_STOCK_TICK, DB_COIN_TICK
+
+
+class QueryTick:
+    def __init__(self, qlist):
+        """
+                    0        1       2        3       4       5       6       7      8      9
+        qlist = [windowQ, soundQ, query1Q, query2Q, teleQ, receivQ, stockQ, coinQ, sstgQ, cstgQ,
+                 tick1Q, tick2Q, tick3Q, tick4Q, tick5Q]
+                   10       11      12     13      14
+        """
+        self.windowQ = qlist[0]
+        self.query2Q = qlist[3]
+        self.con1 = sqlite3.connect(DB_STOCK_TICK)
+        self.con2 = sqlite3.connect(DB_COIN_TICK)
+        self.Start()
+
+    def __del__(self):
+        self.con1.close()
+        self.con2.close()
+
+    def Start(self):
+        while True:
+            query = self.query2Q.get()
+            if query[0] == 1:
+                try:
+                    if len(query) == 2:
+                        for code in list(query[1].keys()):
+                            query[1][code].to_sql(code, self.con1, if_exists='append', chunksize=1000)
+                        self.windowQ.put([ui_num['S단순텍스트'], '시스템 명령 실행 알림 - 틱데이터 저장 완료'])
+                    elif len(query) == 4:
+                        query[1].to_sql(query[2], self.con1, if_exists=query[3], chunksize=1000)
+                except Exception as e:
+                    self.windowQ.put([ui_num['S단순텍스트'], f'시스템 명령 오류 알림 - to_sql {e}'])
+            elif query[0] == 2:
+                try:
+                    for ticker in list(query[1].keys()):
+                        query[1][ticker].to_sql(ticker, self.con1, if_exists='append', chunksize=1000)
+                    self.windowQ.put([ui_num['C단순텍스트'], '시스템 명령 실행 알림 - 틱데이터 저장 완료'])
+                except Exception as e:
+                    self.windowQ.put([ui_num['C단순텍스트'], f'시스템 명령 오류 알림 - to_sql {e}'])
