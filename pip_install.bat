2704ce48a468c8ffbb5883bc273f793375e67aa2
DELL-13\heeji <heejin.jeong@gmx.de>
2021-10-03 04:21:11 +0900

    update pip command

--- pip_install.bat
+++ pip_install.bat
@@ -1,10 +1,2 @@
-pip install pyttsx3
-pip install pywin32
-pip install pyupbit
-pip install PyQt5
-pip install psutil
-pip install telegram
-pip install python-telegram-bot
-pip install pandas
-
+pip install psutil pyqt5 pandas pyttsx3 pyqtgraph matplotlib BeautifulSoup4 lxml python-telegram-bot pyupbit websockets==9.1
 echo "please update open api/project path in utility/setting.py"
