ab90152026675f1a58850d392a334a86ca526536
yuseonho <youseonho@naver.com>
2021-10-04 01:21:52 +0900

    Update pip_install.bat

--- pip_install.bat
+++ pip_install.bat
@@ -1,2 +1,2 @@
-pip install psutil pyqt5 pandas pyttsx3 pyqtgraph matplotlib BeautifulSoup4 lxml python-telegram-bot pyupbit websockets==9.1
+pip install psutil pyqt5 pandas pyttsx3 matplotlib python-telegram-bot pyupbit websockets==9.1
 echo "please update open api/project path in utility/setting.py"
