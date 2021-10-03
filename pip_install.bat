529393c027ca4638cb29f5bf6f7077efef7a9a12
yuseonho <youseonho@naver.com>
2021-10-04 02:00:28 +0900

    Update pip_install.bat

--- pip_install.bat
+++ pip_install.bat
@@ -1,2 +1,14 @@
+chcp 65001
 pip install psutil pyqt5 pandas pyttsx3 matplotlib python-telegram-bot pyupbit websockets==9.1
-echo "please update open api/project path in utility/setting.py"
+@echo off
+@echo ====================================================================================
+@echo 다음 두개의 파일내에서 디렉토리 경로를 수정해야 프로그램이 올바르게 작동합니다.
+@echo ====================================================================================
+@echo utility/setting.py
+@echo OPENAPI_PATH = 'D:/OpenAPI'
+@echo SYSTEM_PATH = 'D:/PythonProjects/PyStockTrader'
+@echo ====================================================================================
+@echo pystocktrader.bat
+@echo cd /D D:/PythonProjects/PyStockTrader
+@echo ====================================================================================
+pause
