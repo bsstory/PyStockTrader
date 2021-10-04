5987af299bffdfb265f8caa3e308e244b8b99e35
yuseonho <youseonho@naver.com>
2021-10-04 10:56:43 +0900

    보유시간 30분 후 청산 기능 추가 Alt + T

--- coin/trader_upbit.py
+++ coin/trader_upbit.py
@@ -271,7 +271,7 @@ class TraderUpbit(QThread):
         if self.dict_bool['보유시간기준청산']:
             df = self.df_cj[(self.df_cj['종목명'] == ticker) & (self.df_cj['주문구분'] == '매수')]
             buy_time = df['체결시간'][df.index[0]]
-            if now > timedelta_sec(1800, strp_time('%Y%m%d%H%M%S%f', buy_time)):
+            if now() > timedelta_sec(1800, strp_time('%Y%m%d%H%M%S%f', buy_time)):
                 self.Sell(ticker, c, oc)
 
     def JangoCheongsan(self):
